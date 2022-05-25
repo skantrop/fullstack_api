a6ab477d38af82553911e41693ecd9fa47651492
aanastasiyatuz <aanastasiyatuz@gmail.com>
2022-05-26 01:49:32 +0600

    heroku

--- account/models.py
+++ account/models.py
@@ -8,7 +8,6 @@ from shop import settings
 from django.dispatch import receiver
 from django.urls import reverse
 
-from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
 from decouple import config
 
@@ -130,20 +129,4 @@ class User(AbstractBaseUser):
             message,
             'test@gmail.com',
             [self.email]
-        )
-
-    @receiver(reset_password_token_created)
-    def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-        email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'),
-                                                       reset_password_token.key)
-
-        send_mail(
-            # title:
-            "Password Reset for {title}".format(title="Some website title"),
-            # message:
-            email_plaintext_message,
-            # from:
-            "noreply@somehost.local",
-            # to:
-            [reset_password_token.user.email]
         )
